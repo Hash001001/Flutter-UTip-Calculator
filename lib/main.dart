59c27671af4a1e46cfef4fb7e2085ba3e1749b78
muhammad.asher <Asher@321>
2026-03-27 16:14:35 +0500

    increament and decreament functionlity implemented

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,6 @@ void main() {
   runApp(MyHome());
 }
 
-
 class MyHome extends StatefulWidget {
   const MyHome({super.key});
 
@@ -13,8 +12,26 @@ class MyHome extends StatefulWidget {
 }
 
 class _MyHomeState extends State<MyHome> {
-
   bool _toggleValue = false;
+
+  final _textController = TextEditingController();
+
+  int _personCount = 1;
+
+
+  void _incrementPersonCount(){
+    setState(() {
+      _personCount++;
+    });
+  }
+
+  void _decrementPersonCount(){
+    setState(() {
+      if(_personCount > 1){
+        _personCount--;
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -22,28 +39,27 @@ class _MyHomeState extends State<MyHome> {
     var style = theme.textTheme.titleLarge!.copyWith(
       color: theme.colorScheme.onPrimary,
       fontWeight: FontWeight.bold,
-      fontSize: 20
+      fontSize: 20,
     );
-
-
 
     return MaterialApp(
       title: "UTip",
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.purple),
-        useMaterial3: true
+        useMaterial3: true,
       ),
       home: Scaffold(
         appBar: AppBar(
           title: Text("UTip"),
           actions: [
             Switch(
-              value: (_toggleValue), onChanged: (onChanged){
-              setState(() {
-                _toggleValue = onChanged;
-                
-              });
-            })
+              value: (_toggleValue),
+              onChanged: (onChanged) {
+                setState(() {
+                  _toggleValue = onChanged;
+                });
+              },
+            ),
           ],
         ),
         body: Column(
@@ -52,59 +68,102 @@ class _MyHomeState extends State<MyHome> {
             Padding(
               padding: const EdgeInsets.all(8.0),
               child: Container(
-                height: 150,
                 decoration: BoxDecoration(
                   color: Colors.deepPurple[500]!.withOpacity(0.6),
-                  borderRadius: BorderRadius.circular(6)
+                  borderRadius: BorderRadius.circular(6),
                 ),
-                padding: EdgeInsets.all(10),
+                padding: EdgeInsets.all(19),
                 child: Column(
                   mainAxisAlignment: MainAxisAlignment.center,
                   crossAxisAlignment: CrossAxisAlignment.center,
                   children: [
-                    Text("Total Per Person",
-                     style: style),
-                    Text("\$230", style: style.copyWith(
-                      fontSize: 25,
-                      color: theme.colorScheme.onPrimary
-                    ),)
-
+                    Text("Total Per Person", style: style),
+                    Text(
+                      "\$230",
+                      style: style.copyWith(
+                        fontSize: 25,
+                        color: theme.colorScheme.onPrimary,
+                      ),
+                    ),
                   ],
                 ),
               ),
-),
+            ),
 
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Container(
+                decoration: BoxDecoration(
+                  border: BoxBorder.all(
+                    color: Colors.purple.withOpacity(0.6),
+                    width: 1.5,
+                  ),
+                  borderRadius: BorderRadius.circular(6),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: [
+                      SizedBox(
+                      height:   8
+                      ),
+                      TextField(
+                        controller: _textController,
+                        keyboardType: TextInputType.number,
+                        onChanged: (value) {},
 
-Expanded(child: Padding(
-  padding: const EdgeInsets.all(10.0),
-  child: Container(
-    decoration: BoxDecoration(
-      border: BoxBorder.all(color: Colors.purple.withOpacity(0.6), width: 1.5),
-      borderRadius: BorderRadius.circular(6)
-    ),
-    child: Padding(
-      padding: const EdgeInsets.all(10.0),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          TextField(
-      
-  
-            onChanged: (value){
+                        decoration: InputDecoration(
+                          prefixIcon: Icon(
+                            Icons.attach_money,
+                            color: theme.colorScheme.primary,
+                          ),
+                          label: Text("Bill Amount"),
+                          border: OutlineInputBorder(),
+                        ),
+                      ),
 
-            },
+                      //spaces
+                      SizedBox(height: 20),
+                      //split bill widget
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            "Split",
+                            style: theme.textTheme.titleMedium!.copyWith(
+                              color: theme.colorScheme.primary,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
 
-            decoration: InputDecoration(
-            
-              label: Text("Bill Amount"), border: OutlineInputBorder(borderRadius: BorderRadius.circular(6))),
-          )
-        ],
-      
-      ),
-    )
-  ),
-),)
+                          Row(
+                            children: [
+                              IconButton(
+                                icon: Icon(Icons.remove),
+                                color: theme.colorScheme.primary, onPressed: _decrementPersonCount,
+                              ),
 
+                              Text(
+                                "$_personCount",
+                                style: theme.textTheme.titleMedium!.copyWith(
+                                  color: theme.colorScheme.primary,
+                                ),
+                              ),
+
+                              IconButton(
+                                icon: Icon(Icons.add),
+                                color: theme.colorScheme.primary, onPressed: _incrementPersonCount,
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
           ],
         ),
       ),
