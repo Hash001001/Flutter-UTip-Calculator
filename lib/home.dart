6367f4ff5277193ff239f0b974a1bd26a11155dc
muhammad.asher <Asher@321>
2026-03-27 18:12:49 +0500

    Bill amount text field refactor

--- lib/home.dart
+++ lib/home.dart
@@ -105,20 +105,9 @@ class _MyHomeState extends State<MyHome> {
                     crossAxisAlignment: CrossAxisAlignment.stretch,
                     children: [
                       SizedBox(height: 8),
-                      TextField(
-                        controller: _textController,
-                        keyboardType: TextInputType.number,
-                        onChanged: (value) {},
-
-                        decoration: InputDecoration(
-                          prefixIcon: Icon(
-                            Icons.attach_money,
-                            color: theme.colorScheme.primary,
-                          ),
-                          label: Text("Bill Amount"),
-                          border: OutlineInputBorder(),
-                        ),
-                      ),
+                      BillAmountField(textController: _textController, theme: theme, amount: "100", onChanged: (value) {
+                        
+                      },),
 
                       //spaces
                       SizedBox(height: 20),
@@ -190,6 +179,39 @@ SizedBox(height: 5,),
             ),
           ],
         ),
+      ),
+    );
+  }
+}
+
+class BillAmountField extends StatelessWidget {
+  const BillAmountField({
+    super.key,
+    required TextEditingController textController,
+    required this.theme, required this.amount, this.onChanged,
+  }) : _textController = textController;
+
+  final TextEditingController _textController;
+  final ThemeData theme;
+  final String amount; 
+  final ValueChanged<String>? onChanged;
+
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      controller: _textController,
+      keyboardType: TextInputType.number,
+      onChanged: (value) {
+        onChanged?.call(value);
+      },
+      decoration: InputDecoration(
+        prefixIcon: Icon(
+          Icons.attach_money,
+          color: theme.colorScheme.primary,
+        ),
+        label: Text("Bill Amount"),
+        border: OutlineInputBorder(),
       ),
     );
   }
